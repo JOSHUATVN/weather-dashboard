1d3b794700a6a2a9471a27ea6bc9c2cb20677490
JOSHUATVN <80295120+JOSHUATVN@users.noreply.github.com>
2021-05-02 16:12:08 -0500

    added localStorage and li ul functions in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,52 @@
-let key = "e41489916c332111126c216d64f6a5f9"
-let cityLists = $("#list-city")
+let key = "e41489916c332111126c216d64f6a5f9";
+let cityLists = $("#list-city");
 let city = [];
+
+function init() {
+  let savedCities = JSON.parse(localStorage.getItem("city"));
+
+  if (savedCities !== null) {
+    city = savedCities;
+  }
+
+  getCities()
+}
+
+
+
+function getCities () {
+    cityLists.empty();
+
+    for (let i =0; i < city.length; i++) {
+        let cities = city[i];
+        let li = $('<li>').text(cities);
+        li.attr("id", "listC");
+        li.attr("data-city", cities);
+        li.atrr("class", "list-group-item");
+        console.log(li);
+        cityLists.prepend(li);
+    }
+
+    if (cities) {
+        return
+    } else {
+        getWeatherResponse(cities)
+    };
+}
+
+
+
+function savedCities () {
+    localStorage.setItem ('city', JSON.stringify(city));
+    console.log(localStorage);
+};
+
+
+
+
+
+// click for li in ul
+$(document).on("click", "", function () {
+  let currentCity = $(this).attr("data-city");
+  getWeatherResponse(currentCity);
+});
